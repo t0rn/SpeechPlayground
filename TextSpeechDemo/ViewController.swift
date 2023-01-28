505e699cedc6a88fc80acbbd0f2a3d330b198534
Alexey Ivanov <aleksey.ivanov1@gmail.com>
2023-01-28 12:08:20 +0100

    Initial Commit

---
+++ TextSpeechDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextSpeechDemo
+//
+//  Created by Alexey Ivanov on 28/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
