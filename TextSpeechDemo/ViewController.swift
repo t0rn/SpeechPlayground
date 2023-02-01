a0cf8a6e1980c800f3d41302272e4e1d08b97e3f
einvolko <124045476+einvolko@users.noreply.github.com>
2023-02-01 20:00:56 +0300

    Ver1.0

--- TextSpeechDemo/ViewController.swift
+++ TextSpeechDemo/ViewController.swift
@@ -6,10 +6,70 @@
 //
 
 import UIKit
+import ARKit
+import AVFoundation
 //How to convert text to speech
 //https://www.hackingwithswift.com/example-code/media/how-to-convert-text-to-speech-using-avspeechsynthesizer-avspeechutterance-and-avspeechsynthesisvoice
 class ViewController: UIViewController {
-
+    let synthesizer = AVSpeechSynthesizer()
+    var language = "ru-RU"
+    
+    
+    @IBOutlet weak var textField: UITextView!
+    
+    @IBAction func RussiaButton(_ sender: Any) {
+        language = "ru-RU"
+    }
+    
+    @IBAction func spanishButton(_ sender: Any) {
+        language = "es-ES"
+    }
+    
+    @IBOutlet weak var pitchSlider: UISlider!
+    
+    
+    @IBOutlet weak var rateSlider: UISlider!
+    
+    
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    
+    @IBOutlet weak var Pitch: UILabel!
+    
+    
+    @IBOutlet weak var Rate: UILabel!
+    
+    
+    @IBOutlet weak var Volume: UILabel!
+    
+    @IBAction func speakButton(_ sender: Any) {
+        let utterance = AVSpeechUtterance(string:textField.text)
+        utterance.voice = AVSpeechSynthesisVoice(language: language)
+        utterance.rate = rateSlider.value
+        utterance.pitchMultiplier = pitchSlider.value
+        utterance.volume = volumeSlider.value
+    
+        synthesizer.speak(utterance)
+    }
+    
+    
+    @IBAction func PitchValueChanged(_ sender: Any) {
+        Pitch.text = String(format:"%.1f", pitchSlider.value)
+    }
+    
+    @IBAction func RateValueChanged(_ sender: Any) {
+        Rate.text = String(format:"%.1f", rateSlider.value)
+    }
+    
+    @IBAction func VolumeValueChanged(_ sender: Any) {
+        Volume.text = String(format:"%.1f", volumeSlider.value)
+    }
+    
+    
+    
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
